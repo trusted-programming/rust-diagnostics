627be8902b49091d048e1403bd7b0cc0882f4b1e
Yijun Yu <y.yu@open.ac.uk>
2023-02-12 18:20:59 +0000

    update LOC

--- crates/count/src/lib.rs
+++ crates/count/src/lib.rs
@@ -5,14 +5,19 @@ pub mod test;
 
 use warning::Warning;
 use persistence::load_map;
+use persistence::load_loc_map;
 
 pub fn counting() 
 {
     let map: BTreeMap<String, Vec<Warning>> = load_map();
+    let loc_map: BTreeMap<String, usize> = load_loc_map();
     let mut s: usize = 0;
     map.iter().for_each(|(k, v)| {
-        println!("{k}: {}", v.len());
+        let segs = k.split("->").collect::<Vec<&str>>();
+        let k0 = format!("{}->{}", segs[0], segs[1]);
+        let k1 = format!("{}->{}->loc", segs[0], segs[1]);
+        let loc = loc_map.get(&k1).unwrap();
+        println!("{k0}: {}, {}", v.len(), loc);
         s += v.len()
     });
-    println!("==================================== {s}");
 }
